fddcad15743fbab4f0349d1394d63041a8028b51
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2019-09-25 22:24:30 +0200

    Dum little cli-arg parsing program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+
+// Setup command line argument options
+#[derive(StructOpt)]
+struct Cli {
+    command: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf
+}
+
 fn main() {
     println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("Command: {}", args.command);
+    println!("Path: {:?}", args.path);
 }
