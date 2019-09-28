2fcc14e541f8751089fc50d89fa3d322a369bbb3
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2019-09-28 19:38:55 +0200

    Learned how to match a complex pattern

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
-// use super_pipe as sup;
+use super_pipe as sup;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Super Pipelines for your filesystem")]
@@ -20,11 +20,39 @@ enum Sup {
     Delete {
         #[structopt(parse(from_os_str))]
         path: PathBuf,
+    },
+    /// Configure super pipe
+    Config(Config)
+}
+
+#[derive(Debug, StructOpt)]
+enum Config {
+    /// Display a listing of configuration values
+    List,
+    /// Set a configuration value
+    Set {
+        key: String,
+        value: String
+    },
+    /// Display a single configuration value
+    Get {
+        key: String
     }
 }
 
 fn main() {
     let opt = Sup::from_args();
     println!("{:?}", opt);
-}
 
+    // Dispatch on the sub-commands
+    let foo = 
+        match opt {
+            Sup::Add { path, commands: cmds } => {
+                println!("Path: {:?}", path);
+                println!("Commands: {:?}", cmds);
+                1
+            },
+            _ => 2
+        };
+    println!("{}", foo);
+}
