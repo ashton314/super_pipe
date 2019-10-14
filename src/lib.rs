7d1b5ceff4ff7ad3b6dc75c3906e67685a9360da
Ashton Wiersdorf <ashton.wiersdorf@twentybn.com>
2019-10-14 21:05:20 +0200

    WIP: getting commands to run

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod store;
 use std::process::Command;
 use std::str;
+// use std::path;
+use std::fs;
+use std::io::Write;
 
 pub fn add_path(path: std::path::PathBuf, cmds: Vec<String>) {
     println!("Adding path: {:?}, cmds: {:?}", path, cmds);
@@ -40,9 +43,19 @@ pub fn run_pipeline(id: u32) {
 
     println!("Pipe: {:?}", pipe);
 
-    let output = Command::new("eval")
-        .env("SUP_SRC", pipe.path)
-        .args(&pipe.cmds)
+    let mut fh = fs::File::create("/tmp/sup_cmd")
+        .expect("Couldn't open tmp cmd file");
+
+    for {
+    }
+    pipe.cmds.iter().map(|cmd| {
+        write!(fh, "{}", cmd);
+    });
+
+    let output = Command::new("whoami")
+        // .env("SUP_SRC", pipe.path)
+        // .args(&["echo", "hello world"])
+        // .args(&pipe.cmds)
         .output()
         .expect("Failed to execute process");
 
